f8ae8a5cf501c5f564c847db75a5c46dfff12177
hyun-10 <111376655+hyun-10@users.noreply.github.com>
2022-08-18 16:43:48 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamilt as st
 
-from pages import pages
+from pages import pages as p3
 
 
 
@@ -14,6 +14,6 @@ st.title('Movie Project')
 
 app.add_page('메인 페이지')
 app.add_page('영화 정보')
-app.add_page('영화인 정보')
+app.add_page('영화인 정보', p3.app)
 app.add_page('영화추천')
 app.add_page('가상 캐스팅')
