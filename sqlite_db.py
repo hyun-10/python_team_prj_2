de7a325130946304112805cceaa4592d115c581e
hyun-10 <111376655+hyun-10@users.noreply.github.com>
2022-08-18 11:29:06 +0900

    Update sqlite_db.py

--- sqlite_db.py
+++ sqlite_db.py
@@ -10,7 +10,7 @@ cursor = connect.cursor();
 input = st.text_input("영화인 이름")
 
 
-cursor.execute("""select peopleCd, peopleNmEn ,repRoleNm, filmoNames from all_movie_people_list where peopleNm in (input,'')""")
+cursor.execute("""select peopleCd, peopleNmEn ,repRoleNm, filmoNames from all_movie_people_list where peopleNm in ('')""")
 for i in cursor:
     st.write(i)
 
