20e6cad3e0a14ea69c3784dc5e6be74a1abd19b0
hyun-10 <111376655+hyun-10@users.noreply.github.com>
2022-08-18 17:39:25 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 import streamlit as st
 from add_pages import pages
-from pages import pages3 as p1
-from pages import pages3 as p2
+from pages import pages1 as p1
+from pages import pages2 as p2
 from pages import pages3 as p3
-from pages import pages3 as p4
-from pages import pages3 as p5
+from pages import pages4 as p4
+from pages import pages5 as p5
 
 
 
@@ -15,7 +15,7 @@ from pages import pages3 as p5
 
 app=pages()
 
-st.title('Movie Project')
+st.title('Movie Project'p1.app)
 st.add_page('메인 페이지')
 st.add_page('영화 정보')
 st.add_page('영화인 정보',p3.app)
