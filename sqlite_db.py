19228f6fe280bde2c1e8643f5f7689aa8e2836b9
hyun-10 <111376655+hyun-10@users.noreply.github.com>
2022-08-18 11:23:17 +0900

    Update sqlite_db.py

--- sqlite_db.py
+++ sqlite_db.py
@@ -5,9 +5,10 @@ connect = sqlite3.connect('all_movie_people_.db', isolation_level=None)
 
 
 cursor = connect.cursor();
+st.write('영화인검색')
 
-
-
+input = st.text_input("영화인 이름")
+st.write(input)
 
 cursor.execute("""select peopleCd, peopleNmEn ,repRoleNm, filmoNames from all_movie_people_list where peopleNm in ('이훈')""")
 for i in cursor:
