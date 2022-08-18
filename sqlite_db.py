b2487935bd786b836a889efc06a8868381e07d8a
hyun-10 <111376655+hyun-10@users.noreply.github.com>
2022-08-18 11:08:01 +0900

    Update sqlite_db.py

--- sqlite_db.py
+++ sqlite_db.py
@@ -7,9 +7,9 @@ connect = sqlite3.connect('all_movie_people_.db', isolation_level=None)
 cursor = connect.cursor();
 
 st.write('영화인 검색')
-input = st.text_input("영화인 ")
-st.write(input)
-select = """select peopleCd, peopleNmEn ,repRoleNm, filmoNames from all_movie_people_list where peopleNm in ('서한교')
+input = st.text_input("영화인")
+
+select = """select peopleCd, peopleNmEn ,repRoleNm, filmoNames from all_movie_people_list where peopleNm in (input)
 """
 cursor.execute(select)
 for i in cursor:
