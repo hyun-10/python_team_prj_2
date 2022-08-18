abdfee02da67a5181c7a6e7789307815e86b8a77
hyun-10 <111376655+hyun-10@users.noreply.github.com>
2022-08-19 03:01:20 +0900

    Update page2.py

--- addpages/page2.py
+++ addpages/page2.py
@@ -1,5 +1,11 @@
 import streamlit as st
+st.write('영화정보 조회')
+
 
 def app():
-
-  st.write('page2')
+  connect = sqlite3.connect('movie_value_test1.db', isolation_level=None)
+  cursor = connect.cursor();
+  
+  input = st.text_input("영화 이름")
+  quary_string = f"select peopleCd, peopleNmEn ,repRoleNm, peopleNm ,imgURL ,filmoNames ,peopleNm from movie_people_included_imgURL where peopleNm in ('{input}')"
+  cursor.execute(quary_string)
