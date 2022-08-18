66fa90f4ac8ad4277e0706aa824a964effee879d
hyun-10 <111376655+hyun-10@users.noreply.github.com>
2022-08-18 17:25:04 +0900

    Update app.py

--- app.py
+++ app.py
@@ -13,9 +13,7 @@ app=pages()
 
 st.title('Movie Project')
 
-app.add_page('메인 페이지')
-app.add_page('영화 정보')
+
 app.add_page('영화인 정보', p3.app)
-app.add_page('영화추천')
-app.add_page('가상 캐스팅')
+
 app.run()
