3ef492580213644350f2d7f2b4d1ee9f28c1c9d2
hyun-10 <111376655+hyun-10@users.noreply.github.com>
2022-08-18 10:52:32 +0900

    Update sqlite_db.py

--- sqlite_db.py
+++ sqlite_db.py
@@ -2,16 +2,16 @@ import sqlite3
 
 connect = sqlite3.connect('all_movie_people_.db', isolation_level=None)
 
-print('test1')
+st.write('test1')
 
 cursor = connect.cursor();
 
-print('test2')
+st.write('test2')
 
 select = """select peopleCd, peopleNmEn ,repRoleNm, filmoNames from all_movie_people_list where peopleNm in ('방준석')
 """
 cursor.execute(select)
 for i in cursor:
-    print(i)
+    st.write(i)
 
-print('test3')
+st.write('test3')
