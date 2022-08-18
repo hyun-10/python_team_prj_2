7b73e82ee845588a06857152cf036f146777bd66
hyun-10 <111376655+hyun-10@users.noreply.github.com>
2022-08-18 17:47:36 +0900

    Update page3.py

--- pages/page3.py
+++ pages/page3.py
@@ -1,24 +1,28 @@
 import sqlite3
 import streamlit as st
-connect = sqlite3.connect('movie_people_included_imgURL.db', isolation_level=None)
 
-st.title('영화')
+def app():
+    connect = sqlite3.connect('movie_people_included_imgURL.db', isolation_level=None)
 
-cursor = connect.cursor();
+    st.title('영화')
 
-
-input = st.text_input("영화인 이름")
-
-quary_string = f"select peopleCd, peopleNmEn ,repRoleNm, peopleNm ,imgURL ,filmoNames ,peopleNm from movie_people_included_imgURL where peopleNm in ('{input}')"
+    cursor = connect.cursor();
 
 
+    input = st.text_input("영화인 이름")
+
+    quary_string = f"select peopleCd, peopleNmEn ,repRoleNm, peopleNm ,imgURL ,filmoNames ,peopleNm from movie_people_included_imgURL where peopleNm in ('{input}')"
 
 
-cursor.execute(quary_string)
 
-for peopleCd, peopleNmEn ,repRoleNm, peopleNm ,imgURL ,filmoNames,peopleNm  in cursor:
-    st.write(peopleNm, peopleNmEn)
 
-    st.write(filmoNames)
+    cursor.execute(quary_string)
+
+    for peopleCd, peopleNmEn ,repRoleNm, peopleNm ,imgURL ,filmoNames,peopleNm  in cursor:
+        st.write(peopleNm, peopleNmEn)
+
+        st.write(filmoNames)
     
-    st.image(imgURL,width=150,)
+        st.image(imgURL,width=150,)
+
+    
