6195f970e9a14ca65fa31fd56e98463c23fa67c5
hyun-10 <111376655+hyun-10@users.noreply.github.com>
2022-08-18 11:05:02 +0900

    Update sqlite_db.py

--- sqlite_db.py
+++ sqlite_db.py
@@ -2,16 +2,15 @@ import sqlite3
 import streamlit as st
 connect = sqlite3.connect('all_movie_people_.db', isolation_level=None)
 
-st.write('test1')
+
 
 cursor = connect.cursor();
 
-st.write('test2')
-
-select = """select peopleCd, peopleNmEn ,repRoleNm, filmoNames from all_movie_people_list where peopleNm in ('방준석')
+st.write('영화인 검색')
+input = st.text_input("message")
+select = """select peopleCd, peopleNmEn ,repRoleNm, filmoNames from all_movie_people_list where peopleNm in (input)
 """
 cursor.execute(select)
 for i in cursor:
     st.write(i)
 
-st.write('test3')
