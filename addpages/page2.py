e2791ae523c0f6fe181fdad2eae9202223786dc8
hyun-10 <111376655+hyun-10@users.noreply.github.com>
2022-08-20 02:17:19 +0900

    Update page2.py

--- addpages/page2.py
+++ addpages/page2.py
@@ -10,7 +10,7 @@ def app():
   Category1 = st.columns(5)
   Data_category = Category1[0].checkbox("가족") 
   if Data_category :
-      print('daf') 
+      st.write('daf') 
   Data_category1 = Category1[1].checkbox("공연") 
   Data_category2 = Category1[2].checkbox("공포") 
   
