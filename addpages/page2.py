f25fd8b8b73a7421e983c03821797caa971bcb0c
hyun-10 <111376655+hyun-10@users.noreply.github.com>
2022-08-19 11:35:38 +0900

    Update page2.py

--- addpages/page2.py
+++ addpages/page2.py
@@ -4,11 +4,11 @@ st.write('영화정보 조회')
 
 
 def app():
-  connect = sqlite3.connect('movie_url_2000.db', isolation_level=None)
+  connect = sqlite3.connect('reset3.db', isolation_level=None)
   cursor = connect.cursor();
   
   input = st.text_input("영화 이름")
-  quary_string = f"select img_url, movie_t from url_2000 where movie_t in ('{input}')"
+  quary_string = f"select 영화이름 from reset3 where 영화이름 in ('{input}')"
   cursor.execute(quary_string)
   for img_url, movie_t in cursor:
 
