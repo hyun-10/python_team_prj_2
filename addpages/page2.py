8363f8a51435671e8abb53d966590d0952e464ed
hyun-10 <111376655+hyun-10@users.noreply.github.com>
2022-08-19 14:01:54 +0900

    Update page2.py

--- addpages/page2.py
+++ addpages/page2.py
@@ -7,7 +7,7 @@ def app():
   connect = sqlite3.connect('reset3.db', isolation_level=None)
   cursor = connect.cursor();
   
-  Category = st.columns(30)
+  Category = st.columns(6)
   Data_category = Category[0].checkbox("가족") 
   
   Data_category1 = Category[1].checkbox("공연") 
@@ -16,19 +16,7 @@ def app():
   Data_category3 = Category[3].checkbox("다큐멘터리") 
   Data_category4 = Category[4].checkbox("드라마")
   Data_category5 = Category[5].checkbox("멜로")
-  Data_category6 = Category[6].checkbox("뮤지컬")
-  Data_category7 = Category[7].checkbox("미스터리")
-  Data_category8 = Category[8].checkbox("범죄")
-  Data_category9 = Category[9].checkbox("사극")
-  Data_category10 = Category[10].checkbox("서부극")
-  Data_category11 = Category[11].checkbox("성인물")
-  Data_category12 = Category[12].checkbox("스릴러")
-  Data_category13 = Category[13].checkbox("애니메이션")
-  Data_category14 = Category[14].checkbox("액션")
-  Data_category15 = Category[15].checkbox("어드벤처")
-  Data_category16 = Category[16].checkbox("전쟁")
-  Data_category17 = Category[17].checkbox("코미디")
-  Data_category18 = Category[18].checkbox("판타지")
+ 
   
   
   
