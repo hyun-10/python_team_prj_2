b3ff85797b6bcd1987785d5007b5ab510d6916e1
hyun-10 <111376655+hyun-10@users.noreply.github.com>
2022-08-19 15:51:18 +0900

    Update page2.py

--- addpages/page2.py
+++ addpages/page2.py
@@ -41,7 +41,7 @@ def app():
   '''
   
   st.text_input("영화 이름")
-  if family, performance, horror, etc, documentary, drama, melodrama, musical, mystery, crime, historical, western, adult, thriller, animated, action, adventure, war, comedy, fantasy ==1:
+  
       if quary_string = f"select movieCd, movieNm, prdtYear, showTm, prdtStatNM, nations, family, performance, horror, etc, documentary, drama, melodrama, musical, mystery, crime, historical, western, adult, thriller, animated, action, adventure, war, comedy, fantasy,peopleNm, actors, staffs from movie_list where movieNm in ('{input}')"
   curs
   
