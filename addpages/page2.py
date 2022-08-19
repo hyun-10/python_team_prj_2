30a78d9722c9ebc4cc55fa4d7799a84a15fe4f54
hyun-10 <111376655+hyun-10@users.noreply.github.com>
2022-08-19 16:52:13 +0900

    Update page2.py

--- addpages/page2.py
+++ addpages/page2.py
@@ -38,10 +38,10 @@ def app():
   
   input = st.text_input("영화 이름")
   
-  quary_string = f"select movieCd, movieNm, prdtYear, showTm, prdtStatNM, nations, family, performance, horror, etc, documentary, drama, melodrama, musical, mystery, crime, historical, western, adult, thriller, animated, action, adventure, war, comedy, fantasy,peopleNm, actors, staffs from movie_list where movieNm in ('%{input}%')"
-  cursor.execute(quary_string)
-  for 영화이름 in cursor:
-      st.write(영화이름)
+quary_string = f"select movieCd, movieNm, prdtYear, showTm, prdtStatNM, nations, family, performance, horror, etc, documentary, drama, melodrama, musical, mystery, crime, historical, western, adult, thriller, animated, action, adventure, war, comedy, fantasy,peopleNm, actors, staffs from movie_list where movieNm like ('%{input}%')"
+cursor.execute(quary_string)
+for 영화이름 in cursor:
+    st.write(영화이름)
       
 
 
