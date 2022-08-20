22c71e1823020b724802873fd3814c2c890d5e9d
hyun-10 <111376655+hyun-10@users.noreply.github.com>
2022-08-20 16:28:42 +0900

    Update page1.py

--- addpages/page1.py
+++ addpages/page1.py
@@ -13,7 +13,7 @@ def app():
   
   
   #input="select 영화명, 순위, 개봉일, 누적관객수, img_url from box ORDER BY 개봉일 DESC where 영화명 NOT IN(' " " ')"
-  input="select 영화명, 순위, 개봉일, 누적관객수, img_url from box where 영화명 NOT IN(' " " ') ORDER BY 개봉일 DESC "
+  input="select 영화명, 순위, 개봉일, 누적관객수, img_url from box where 영화명 NOT IN(' " " ')"
   cursor.execute(input)
   for i in cursor:
       st.write(i)
