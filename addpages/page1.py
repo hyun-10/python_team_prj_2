d103df8693ea1e8b8ddb09998aec7b51c00c459b
hyun-10 <111376655+hyun-10@users.noreply.github.com>
2022-08-22 08:58:03 +0900

    Update page1.py

--- addpages/page1.py
+++ addpages/page1.py
@@ -31,7 +31,7 @@ def app():
   
   #input = 'SELECT 영화명,개봉일,img_url FROM box WHERE 개봉일 GROUP BY 영화명 BETWEEN 2202-07-24 AND 2202-08-24  ORDER BY 개봉일 DESC '
   
-  input = 'SELECT 영화명,개봉일,img_url FROM box WHERE 개봉일 BETWEEN 1973-07-27 AND 2202-08-24 GROUP BY 영화명 ORDER BY 개봉일 DESC '<=10
+  input = 'SELECT 영화명,개봉일,img_url FROM box WHERE 개봉일 BETWEEN 1973-07-27 AND 2202-08-24 GROUP BY 영화명 ORDER BY 개봉일 DESC LIMIT 0,10'
   cursor.execute(input)
   
   
