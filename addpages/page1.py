f45579cd45e30cd70e8dcea6cfbc2ead39a2c7fa
hyun-10 <111376655+hyun-10@users.noreply.github.com>
2022-08-22 03:07:00 +0900

    Update page1.py

--- addpages/page1.py
+++ addpages/page1.py
@@ -17,7 +17,7 @@ def app():
   
   
   
-  input = 'SELECT 영화명,개봉일,img_url FROM box WHERE 개봉일 NOT IN(null) ORDER BY 개봉일 ASC '
+  input = 'SELECT 영화명,개봉일,img_url FROM box WHERE 개봉일 NOT IN(null)'
   cursor.execute(input)
   for i in cursor:
       st.write(i)
