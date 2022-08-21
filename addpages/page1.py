32f8c5eb40e87597c6b49b6159e610ff77657cfe
hyun-10 <111376655+hyun-10@users.noreply.github.com>
2022-08-22 02:57:14 +0900

    Update page1.py

--- addpages/page1.py
+++ addpages/page1.py
@@ -17,7 +17,7 @@ def app():
   
   
   
-  input = 'SELECT 영화명,개봉일, 누적관객수,img_url FROM box ORDER BY 영화명 ASC'
+  input = 'SELECT 영화명,개봉일, 누적관객수,img_url FROM box ORDER BY 개봉일 ASC'
   cursor.execute(input)
   for i in cursor:
       st.write(i)
