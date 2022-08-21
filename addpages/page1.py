4e9469e378acfebc24d3f562329f0343dbd45338
hyun-10 <111376655+hyun-10@users.noreply.github.com>
2022-08-22 02:02:31 +0900

    Update page1.py

--- addpages/page1.py
+++ addpages/page1.py
@@ -14,7 +14,11 @@ def app():
   
   #input="select 영화명, img_url from box ORDER BY 개봉일 DESC where 영화명 NOT IN(' " " ')"
   #input="select 영화명, 순위, 개봉일, 누적관객수, img_url from box where 영화인,순위,개봉일,누적관객수 NOT IN('"') ORDER BY 영화명 DESC"
-  input = 'select 영화명, 순위, 개봉일, 누적관객수, img_url from box ORDER BY 개봉일 ASC'
+  
+  SELECT * FROM table_name WHERE column_name NOT IN ("value","check");
+
+  
+  input = 'select 영화명, 순위, 개봉일, 누적관객수, img_url from box ORDER BY 개봉일 ASC where 영화명 NOT IN (")'
   cursor.execute(input)
   for i in cursor:
       st.write(i[100:])
