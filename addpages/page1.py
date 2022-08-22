628fb656bd79f646c98fdda2a46fc00da42b5f60
hyun-10 <111376655+hyun-10@users.noreply.github.com>
2022-08-22 15:22:49 +0900

    Update page1.py

--- addpages/page1.py
+++ addpages/page1.py
@@ -16,22 +16,26 @@ def app():
   input = 'SELECT 영화명,개봉일,img_url FROM box WHERE 개봉일 BETWEEN 1973-07-27 AND 2202-08-24 GROUP BY 영화명 ORDER BY 개봉일 DESC limit 5'
   input_1=cursor.execute(input)
   
-  input_2 = 'SELECT 영화명,개봉일,img_url FROM box WHERE 개봉일 BETWEEN 1973-07-27 AND 2202-08-24 GROUP BY 영화명 ORDER BY 개봉일 DESC limit 5,5'
-  input_2=cursor.execute(input_2)
+
   
   col1, col2, col3, col4, col5= st.columns(5)
-  col6, col7, col8, col9, col10 = st.columns(5)
+
   col_list=[col1,col2,col3,col4,col5]
-  col_list_2=[col6, col7, col8, col9, col10]
+
   i=0
-  j=0
+
   for 영화명, 개봉일, img_url in input_1:
       with col_list[i]:
           st.image(img_url,width=130)
           st.write(영화명)
       i+=1
-  
-  
+      
+      
+  input_2 = 'SELECT 영화명,개봉일,img_url FROM box WHERE 개봉일 BETWEEN 1973-07-27 AND 2202-08-24 GROUP BY 영화명 ORDER BY 개봉일 DESC limit 5,5'
+  col6, col7, col8, col9, col10 = st.columns(5)
+  col_list_2=[col6, col7, col8, col9, col10]
+  input_2=cursor.execute(input_2)
+  j=0
   for 영화명, 개봉일, img_url in input_2:
       with col_list_2[j]:
           st.image(img_url,width=130)
