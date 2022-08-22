1e633a5fcdaeb86bce71e58cb0f06374fa91500e
hyun-10 <111376655+hyun-10@users.noreply.github.com>
2022-08-22 15:17:22 +0900

    Update page1.py

--- addpages/page1.py
+++ addpages/page1.py
@@ -14,7 +14,7 @@ def app():
   #input = 'SELECT 영화명,개봉일,img_url FROM box WHERE 개봉일 GROUP BY 영화명 BETWEEN 2202-07-24 AND 2202-08-24  ORDER BY 개봉일 DESC '
   
   input = 'SELECT 영화명,개봉일,img_url FROM box WHERE 개봉일 BETWEEN 1973-07-27 AND 2202-08-24 GROUP BY 영화명 ORDER BY 개봉일 DESC limit 5'
-  input_1cursor.execute(input)
+  input_1=cursor.execute(input)
   
   input_2 = 'SELECT 영화명,개봉일,img_url FROM box WHERE 개봉일 BETWEEN 1973-07-27 AND 2202-08-24 GROUP BY 영화명 ORDER BY 개봉일 DESC limit 0,10'
   input_2=cursor.execute(input_2)
