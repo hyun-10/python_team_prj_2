12c62531851661300471849dfdff66a0625d9fd0
hyun-10 <111376655+hyun-10@users.noreply.github.com>
2022-08-22 14:32:55 +0900

    Update page1.py

--- addpages/page1.py
+++ addpages/page1.py
@@ -16,15 +16,24 @@ def app():
   input = 'SELECT 영화명,개봉일,img_url FROM box WHERE 개봉일 BETWEEN 1973-07-27 AND 2202-08-24 GROUP BY 영화명 ORDER BY 개봉일 DESC limit 5'
   cursor.execute(input)
   
-  col1, col2, col3, col4, col5, col6, col7, col8, col9, col10= st.columns(10)
+  col1, col2, col3, col4, col5= st.columns(5)
   
-  col_list=[col1,col2,col3,col4,col5,col6,col7,col8,col9,col10 ]
+  col_list=[col1,col2,col3,col4,col5]
   i=0
   for 영화명, 개봉일, img_url in cursor:
       with col_list[i]:
           st.image(img_url,width=130,)
           st.write(영화명)
       i+=1
+      
+  col6, col7, col8, col9, col10= st.columns(5)
+  j=0
+  for 영화명, 개봉일, img_url in cursor:
+      with col_list[j]:
+          st.image(img_url,width=130,)
+          st.write(영화명)
+      j+=1
+   
 
 
 
