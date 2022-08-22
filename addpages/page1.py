46f28f9c40052bba1af811cb52fd7ec20e866905
hyun-10 <111376655+hyun-10@users.noreply.github.com>
2022-08-22 15:12:06 +0900

    Update page1.py

--- addpages/page1.py
+++ addpages/page1.py
@@ -21,14 +21,12 @@ def app():
   col_list=[col1,col2,col3,col4,col5]
   col_list_2=[col6, col7, col8, col9, col10]
   i=0
-  j=0
+
   for 영화명, 개봉일, img_url in cursor:
-      with col_list[i]:
+      with col_list_2[i]:
           st.image(img_url,width=130)
           st.write(영화명)
       i+=1
-
-
       
 
 
