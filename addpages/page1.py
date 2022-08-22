ea761765225cfce6e16dafb13c4a654c0ece4b46
hyun-10 <111376655+hyun-10@users.noreply.github.com>
2022-08-22 10:44:36 +0900

    Update page1.py

--- addpages/page1.py
+++ addpages/page1.py
@@ -8,26 +8,7 @@ def app():
   connect = sqlite3.connect('box.db', isolation_level=None)
   cursor = connect.cursor();
   
-  #cursor.execute('select 영화명 from box')
-  #for i in cursor:
-      #st.write(i)
-  
-  
-  #input="select 영화명, img_url from box ORDER BY 개봉일 DESC where 영화명 NOT IN(' " " ')"
-  #input="select 영화명, 순위, 개봉일, 누적관객수, img_url from box where 영화인,순위,개봉일,누적관객수 NOT IN('"') ORDER BY 영화명 DESC"
-  
-  
-  
-  #input = 'SELECT 영화명,개봉일,img_url FROM box WHERE 개봉일 IN (%-%) ORDER BY 영화명 DESC'
-  
-  #input = 'SELECT 영화명,개봉일,img_url FROM box WHERE NOT 개봉일 IS null ORDER BY 영화명 DESC'
-  
-  
-  
-  
-  
-  
-  #input = 'SELECT 영화명,개봉일,img_url FROM box WHERE NOT 개봉일 IS null GROUP BY 영화명 ORDER BY 개봉일 DESC '
+
   
   
   #input = 'SELECT 영화명,개봉일,img_url FROM box WHERE 개봉일 GROUP BY 영화명 BETWEEN 2202-07-24 AND 2202-08-24  ORDER BY 개봉일 DESC '
