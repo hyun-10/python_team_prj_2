4271fa7b38d5749185b26df217ddf7408c117fed
hyun-10 <111376655+hyun-10@users.noreply.github.com>
2022-08-22 16:16:13 +0900

    Update page2.py

--- addpages/page2.py
+++ addpages/page2.py
@@ -38,8 +38,8 @@ def app():
   
   input = st.text_input('')
   
-  #quary_string = f"select movieCd, movieNm, prdtYear, showTm, prdtStatNM, nations, family, performance, horror, etc, documentary, drama, melodrama, musical, mystery, crime, historical, western, adult, thriller, animated, action, adventure, war, comedy, fantasy,peopleNm, actors, staffs, img_url from movie where movieNm in ('{input}')"
-  quary_string= select id, name, address from user;
+  quary_string = f"select movieCd, movieNm, prdtYear, showTm, prdtStatNM, nations, family, performance, horror, etc, documentary, drama, melodrama, musical, mystery, crime, historical, western, adult, thriller, animated, action, adventure, war, comedy, fantasy,peopleNm, actors, staffs, img_url from movie where movieNm in ('{input}')"
+  quary_string=SELECT table_name FROM information_schema.tables
 
   cursor.execute(quary_string)
   for test in cursor:
