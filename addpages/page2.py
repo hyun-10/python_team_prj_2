a5a21efe1ca0edeb5f86a4fe481ceb6569df88ae
hyun-10 <111376655+hyun-10@users.noreply.github.com>
2022-08-24 03:50:02 +0900

    Update page2.py

--- addpages/page2.py
+++ addpages/page2.py
@@ -4,6 +4,7 @@ st.write('영화정보 조회')
 
 
 def app():
+  '''
   connect = sqlite3.connect('movie.db', isolation_level=None)
   cursor = connect.cursor();
   
@@ -50,20 +51,53 @@ def app():
 
         st.image(img_url,width=130)
         st.write(movieNm, peopleNm)
+  '''
+
+  def and_(input,family):
+      family=1
+      quary_string = "select movieNm, peopleNm,img_url from movie where movieNm like '%{0}%' and family=={1} ".format(input,family)
+      cursor.execute(quary_string)
+
+  input = st.text_input('')
+  if input:
+      and_(input)
+
+
+  Category1 = st.columns(5)
+  Data_category = Category1[0].checkbox("가족") 
+  if Data_category:
+      family=1
+      and_(family)
+  Data_category1 = Category1[1].checkbox("공연") 
+  Data_category2 = Category1[2].checkbox("공포") 
+  
+  Data_category3 = Category1[3].checkbox("다큐멘터리") 
+  Data_category4 = Category1[4].checkbox("드라마")
+  Category2 = st.columns(5)
+  Data_category5 = Category2[0].checkbox("멜로")
+  
+  Data_category6 = Category2[1].checkbox("뮤지컬")
+  Data_category7 = Category2[2].checkbox("미스터리")
+  Data_category8 = Category2[3].checkbox("범죄")
+  Data_category9 = Category2[4].checkbox("사극")
+  Category3 = st.columns(5)
+  Data_category10 = Category3[0].checkbox("서부극")
+  Data_category11 = Category3[1].checkbox("성인물")
+  Data_category12 = Category3[2].checkbox("스릴러")
+  Data_category13 = Category3[3].checkbox("애니메이션")
+  Data_category14 = Category3[4].checkbox("액션")
+  Category4 = st.columns(5)
+  Data_category15 = Category4[0].checkbox("어드벤처")
+  Data_category16 = Category4[1].checkbox("전쟁")
+  Data_category17 = Category4[2].checkbox("코미디")
+  Data_category18 = Category4[3].checkbox("판타지")
 
 
 
 
-'''
-        if input:
-        quary_string = "select movieNm, peopleNm,img_url family, performance, horror, etc, documentary, drama, melodrama, musical, mystery, crime, historical, western, adult, thriller, animated, action, adventure, war, comedy, fantasy from movie where movieNm like '%{0}%' "
-        cursor.execute(quary_string)
-    for movieNm,peopleNm,img_url in cursor:
-        
-        st.image(img_url,width=130)
-        st.write(movieNm, peopleNm)
- 
-'''
+  for movieNm,peopleNm,img_url in cursor:
+          st.image(img_url,width=130)
+          st.write(movieNm, peopleNm)
 
 
       
