df0b8db9cec918896058c98adcffee59461a7d4b
hyun-10 <111376655+hyun-10@users.noreply.github.com>
2022-08-23 15:25:20 +0900

    Update page2.py

--- addpages/page2.py
+++ addpages/page2.py
@@ -9,7 +9,8 @@ def app():
   
   Category1 = st.columns(5)
   Data_category = Category1[0].checkbox("가족") 
-
+  if Data_category:
+      quary_string = 'select family from movie movieNm, img_url'
   Data_category1 = Category1[1].checkbox("공연") 
   Data_category2 = Category1[2].checkbox("공포") 
   
