8cb9cca7f01e299c7c9caa94abdd94aaefd19674
hyun-10 <111376655+hyun-10@users.noreply.github.com>
2022-08-27 02:44:21 +0900

    Update page5.py

--- addpages/page5.py
+++ addpages/page5.py
@@ -19,13 +19,13 @@ def app():
   st.write('page5')
   
   
-  #movie = pd.read_csv('db/4p/movie_info.csv')
-  #movie = movie.astype({'영화코드':'str'})
+  movie = pd.read_csv('db/4p/movie_info.csv')
+  movie = movie.astype({'영화코드':'str'})
  
-  data= pd.read_csv('db/4p/action_review_noh_1.csv', names=['code','score','raw_user', 'userCd','user_id', 'user_nick', 'movie', 'genre','review'])
-  for i in ('adult','adventure','animation','comedy','crime','documentary','drama','etc','family','fantasy','fear','history','melo','musical','mystery','thriller','war','western'):
+  #data= pd.read_csv('db/4p/action_review_noh_1.csv', names=['code','score','raw_user', 'userCd','user_id', 'user_nick', 'movie', 'genre','review'])
+  for i in ('action'adult','adventure','animation','comedy','crime','documentary','drama','etc','family','fantasy','fear','history','melo','musical','mystery','thriller','war','western'):
     df = pd.read_csv(f'db/4p/{i}_review_noh_1.csv',names=['code','score','raw_user', 'userCd','user_id', 'user_nick', 'movie', 'genre','review'])
-    movie_reviews = pd.concat([data, df])
+    movie_reviews = pd.concat([movie_reviews, df])
   st.write(movie_reviews)
                  
 
