7f6cd044fa3b7846b0513a0cddc15d25b45b2413
hyun-10 <111376655+hyun-10@users.noreply.github.com>
2022-08-27 04:42:38 +0900

    Update page5.py

--- addpages/page5.py
+++ addpages/page5.py
@@ -34,7 +34,7 @@ def app():
   da_10= pd.read_csv('db/4p/family_review_noh_1.csv')
   da_11= pd.read_csv('db/4p/fantasy_review_noh_1.csv')
   da_12= pd.read_csv('db/4p/fear_review_noh_1.csv')
-  da_13= pd.read_csv('db/4p/history_review_noh_2.csv')
+  da_13= pd.read_csv('db/4p/history_review_noh_3.csv')
   da_14= pd.read_csv('db/4p/melo_review_noh_1.csv')
   da_15= pd.read_csv('db/4p/musical_review_noh_1.csv')
   da_16= pd.read_csv('db/4p/mystery_review_noh_1.csv')
@@ -44,8 +44,8 @@ def app():
   
   #for i in ('adult','adventure','animation','comedy','crime','documentary','drama','etc','family','fantasy','fear','history','melo','musical','mystery','thriller','war','western'):
     #df = pd.read_csv(f'db/4p/{i}_review_noh_1.csv', encoding='utf-8', engine='python')
-  movie_reviews = pd.concat([da_1,da_2])
-  st.write(movie_reviews)
+  #movie_reviews = pd.concat([da_15])
+  st.write(da_15)
   '''
   
   movie_reviews = movie_reviews.astype({'code':'str'})
