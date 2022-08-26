2ce8a12d2f850bd1e7d4015387dfa06b1c88ef6f
hyun-10 <111376655+hyun-10@users.noreply.github.com>
2022-08-27 03:57:35 +0900

    Update page5.py

--- addpages/page5.py
+++ addpages/page5.py
@@ -25,7 +25,7 @@ def app():
   data= pd.read_csv('db/4p/action_review_noh_1.csv', names=['code','score','raw_user', 'userCd','user_id', 'user_nick', 'movie', 'genre','review'])
   
   for i in ('adult','adventure','animation','comedy','crime','documentary','drama','etc','family','fantasy','fear','history','melo','musical','mystery','thriller','war','western'):
-    df = pd.read_csv(f'db/4p/{i}_review_noh_1.csv',names=['code','score','raw_user', 'userCd','user_id', 'user_nick', 'movie', 'genre','review'], encoding='utf-8', engine='python')
+    df = pd.read_csv(f'db/4p/{i}_review_noh_1.csv', encoding='utf-8', engine='python')
     movie_reviews = pd.concat([data, df])
   movie_reviews = movie_reviews.astype({'code':'str'})
   movie_reviews['select_genre'] = movie_reviews['genre'].apply(lambda x : x.split(',')[0])
@@ -74,7 +74,8 @@ def app():
   
   
   
-  
+  mv_reviews = get_review(code,movie,movie_reviews)
+ 
   
   
   
