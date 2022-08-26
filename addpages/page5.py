3d7b0d53869eefe73cc8da7542236854a6f59f04
hyun-10 <111376655+hyun-10@users.noreply.github.com>
2022-08-27 02:46:37 +0900

    Update page5.py

--- addpages/page5.py
+++ addpages/page5.py
@@ -23,6 +23,7 @@ def app():
   movie = movie.astype({'영화코드':'str'})
  
   #data= pd.read_csv('db/4p/action_review_noh_1.csv', names=['code','score','raw_user', 'userCd','user_id', 'user_nick', 'movie', 'genre','review'])
+  movie_reviews=pd.DataFrame()
   for i in ('action','adult','adventure','animation','comedy','crime','documentary','drama','etc','family','fantasy','fear','history','melo','musical','mystery','thriller','war','western'):
     df = pd.read_csv(f'db/4p/{i}_review_noh_1.csv',names=['code','score','raw_user', 'userCd','user_id', 'user_nick', 'movie', 'genre','review'])
     movie_reviews = pd.concat([movie_reviews, df])
