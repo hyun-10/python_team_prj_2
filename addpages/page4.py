d3c6266e8746b2660df25b581c61221f8a93ab4c
hyun-10 <111376655+hyun-10@users.noreply.github.com>
2022-08-27 14:38:54 +0900

    Update page4.py

--- addpages/page4.py
+++ addpages/page4.py
@@ -11,7 +11,7 @@ import sqlite3
 
 def app():
   #genre_='fantasy'
-  genre_ = st.multiselect(['family','performance' ,'horror','etc','documentary','drama','melodrama','musical','mystery','crime','historical','western','adult','thriller','animated','action','adventure','war','comedy','fantasy'],options)
+  genre_ = st.multiselect(['family','performance' ,'horror','etc','documentary','drama','melodrama','musical','mystery','crime','historical','western','adult','thriller','animated','action','adventure','war','comedy','fantasy'])
 
   #puid= 'adiv****'
   puid = st.text_input('')
