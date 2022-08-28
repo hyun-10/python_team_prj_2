fae5cc7552d3b69ba3ed2eddca3af7402187050f
hyun-10 <111376655+hyun-10@users.noreply.github.com>
2022-08-28 21:48:35 +0900

    Update page4.py

--- addpages/page4.py
+++ addpages/page4.py
@@ -14,10 +14,10 @@ def app():
   genre_ = st.selectbox('장르를 선택하세요',('action','adult','adventure','animation','comedy','crime','documentary','drama','etc','family','fantasy','fear','history','melo','musical','mystery','thriller','war','western'))
   genre_ = genre_
 
-  puid = st.text_input('adiv****', key=puid)
+  puid = st.text_input('adiv****', key=1)
                     
 
-  punick = st.text_input('푸른불',key=punick)
+  punick = st.text_input('푸른불',key=2)
 
   puid=puid[0:4]+'****'
 
