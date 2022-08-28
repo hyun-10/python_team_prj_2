b6e54976c32857192feb91b5eceb3203e1e65ed7
hyun-10 <111376655+hyun-10@users.noreply.github.com>
2022-08-28 21:52:40 +0900

    Update page4.py

--- addpages/page4.py
+++ addpages/page4.py
@@ -18,68 +18,68 @@ def app():
                     
 
   punick = st.text_input('푸른불',key=2)
+  if punick:
+    puid=puid[0:4]+'****'
 
-  puid=puid[0:4]+'****'
-
-  def full_data_learning(genre_):
-    col = 'item rating user'
-    reader = Reader(line_format=col, sep=',', rating_scale=(0,10))
+    def full_data_learning(genre_):
+      col = 'item rating user'
+      reader = Reader(line_format=col, sep=',', rating_scale=(0,10))
  
-    count = 0
-    while True :
-        try :
-            data_folds = DatasetAutoFolds(ratings_file=f'db/4p/{genre_}_review_noh_{str(count)}.csv', reader=reader)
-            trainset = data_folds.build_full_trainset()
-            algo = SVD(n_epochs=5, n_factors=500, random_state=0)
-            algo.fit(trainset)
-            break
-        except :
-            count += 1
-    return algo
+      count = 0
+      while True :
+          try :
+              data_folds = DatasetAutoFolds(ratings_file=f'db/4p/{genre_}_review_noh_{str(count)}.csv', reader=reader)
+              trainset = data_folds.build_full_trainset()
+              algo = SVD(n_epochs=5, n_factors=500, random_state=0)
+              algo.fit(trainset)
+              break
+          except :
+              count += 1
+      return algo
 
-  algo = full_data_learning(genre_)
+    algo = full_data_learning(genre_)
   
-  def mvCd_of_unshow(genre_,puid,punick):
-    data = pd.read_csv(f'db/4p/{genre_}_review_noh_1.csv', names=['code','score','raw_user', 'userCd','user_id', 'user_nick', 'movie', 'genre','review'])
-    str_expr = '(user_id == @puid) and (user_nick == @punick)'
-    total = data.code.unique()
-    query = data.query(str_expr)['code'].unique()
-    indexes = [np.where(total==i)[0][0] for i in query]
-    df_q =np.delete(total,indexes)
-    return df_q
+    def mvCd_of_unshow(genre_,puid,punick):
+      data = pd.read_csv(f'db/4p/{genre_}_review_noh_1.csv', names=['code','score','raw_user', 'userCd','user_id', 'user_nick', 'movie', 'genre','review'])
+      str_expr = '(user_id == @puid) and (user_nick == @punick)'
+      total = data.code.unique()
+      query = data.query(str_expr)['code'].unique()
+      indexes = [np.where(total==i)[0][0] for i in query]
+      df_q =np.delete(total,indexes)
+      return df_q
   
-  unshow_mvCd_array = mvCd_of_unshow(genre_,puid,punick)
+    unshow_mvCd_array = mvCd_of_unshow(genre_,puid,punick)
   
-  def prediction(algo,uid, uia):
-    pred = [algo.predict(uid=str(uid), iid=str(isbn)) for isbn in uia]
-    ests = lambda x : x.est
-    pred.sort(key=ests, reverse=True)
-    top10_pred = pred[:10]
-    isbn_est_list = [(pr.iid, pr.est) for pr in top10_pred]
-    return isbn_est_list
+    def prediction(algo,uid, uia):
+      pred = [algo.predict(uid=str(uid), iid=str(isbn)) for isbn in uia]
+      ests = lambda x : x.est
+      pred.sort(key=ests, reverse=True)
+      top10_pred = pred[:10]
+      isbn_est_list = [(pr.iid, pr.est) for pr in top10_pred]
+      return isbn_est_list
   
-  user_mvrating_est_list = prediction(algo,puid,unshow_mvCd_array)
+    user_mvrating_est_list = prediction(algo,puid,unshow_mvCd_array)
   
-  def get_recomendationmovie(prediction):
-    movies = pd.read_table('db/4p/movie_info.csv', sep=',', encoding='utf-8', engine='python')
-    mvCd_list = []
-    for i in prediction :
-      mvCd_list.append(i[0])
-    str_expr='영화코드 in @mvCd_list'
-    df_q = movies.query(str_expr)
-  #추천순위로 정렬하기
-    df_q['rank']=0
-    rank=1
-    for i in prediction:      
-      index = df_q.index[df_q['영화코드'] == i[0]]
-      df_q.loc[index,'rank']=rank
-      rank=rank+1
-    return df_q
+    def get_recomendationmovie(prediction):
+      movies = pd.read_table('db/4p/movie_info.csv', sep=',', encoding='utf-8', engine='python')
+      mvCd_list = []
+      for i in prediction :
+        mvCd_list.append(i[0])
+      str_expr='영화코드 in @mvCd_list'
+      df_q = movies.query(str_expr)
+    #추천순위로 정렬하기
+      df_q['rank']=0
+      rank=1
+      for i in prediction:      
+        index = df_q.index[df_q['영화코드'] == i[0]]
+        df_q.loc[index,'rank']=rank
+        rank=rank+1
+      return df_q
   
-  user_movie_recomendation_df = get_recomendationmovie(user_mvrating_est_list)
+    user_movie_recomendation_df = get_recomendationmovie(user_mvrating_est_list)
 
-  user_movie_recomendation_df = user_movie_recomendation_df.set_index('rank')
-  st.dataframe(user_movie_recomendation_df.sort_index())
+    user_movie_recomendation_df = user_movie_recomendation_df.set_index('rank')
+    st.dataframe(user_movie_recomendation_df.sort_index())
   
   '''
   genre_='fantasy'
@@ -97,3 +97,6 @@ def app():
   user_movie_recomendation_df_.sort_index()
   st.dataframe(user_movie_recomendation_df_)
   '''
+
+
+  
