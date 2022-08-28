74eb86874a1f32500efc97335ed9c3d3fc8eb231
hyun-10 <111376655+hyun-10@users.noreply.github.com>
2022-08-29 01:08:37 +0900

    Update page4.py

--- addpages/page4.py
+++ addpages/page4.py
@@ -80,4 +80,4 @@ def app():
 
     user_movie_recomendation_df = user_movie_recomendation_df.set_index('rank')
     st.dataframe(user_movie_recomendation_df.sort_index())
-    st.dataframe(ursr_movie_recomendation_df.loc['1'])
+    st.dataframe(ursr_movie_recomendation_df.loc[[1]])
