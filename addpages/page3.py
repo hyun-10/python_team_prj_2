3326fd1d9f54d973d68ce5e742a11d683ec19ec3
hyun-10 <111376655+hyun-10@users.noreply.github.com>
2022-08-29 09:35:19 +0900

    Update page3.py

--- addpages/page3.py
+++ addpages/page3.py
@@ -9,7 +9,7 @@ def app():
   quary_string = f"select peopleCd, peopleNmEn ,repRoleNm, peopleNm ,imgURL ,filmoNames ,peopleNm from movie_people_included_imgURL where peopleNm in ('{input}')"
   cursor.execute(quary_string)
   
-  col1, col2, = st.columns(3,gap="small")
+  col1, col2, col3= st.columns(3,gap="small")
   for peopleCd, peopleNmEn ,repRoleNm, peopleNm ,imgURL ,filmoNames,peopleNm  in cursor:
       if peopleNmEn == None : 
           st.write(peopleNm)
