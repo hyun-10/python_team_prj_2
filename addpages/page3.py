f29e8470b25bbc472d8d1b9f856af2da0793da65
hyun-10 <111376655+hyun-10@users.noreply.github.com>
2022-08-29 09:52:24 +0900

    Update page3.py

--- addpages/page3.py
+++ addpages/page3.py
@@ -9,30 +9,20 @@ def app():
   quary_string = f"select peopleCd, peopleNmEn ,repRoleNm, peopleNm ,imgURL ,filmoNames ,peopleNm from movie_people_included_imgURL where peopleNm in ('{input}')"
   cursor.execute(quary_string)
   
-  col1, col2, col3= st.columns(3,gap="small")
+  col1, col2, col4, col5, col6, col7, col8, col9, col0= st.columns(10,gap="small")
+  col_list=[col1, col2, col4, col5, col6, col7, col8, col9, col0]
+  j=0
   for peopleCd, peopleNmEn ,repRoleNm, peopleNm ,imgURL ,filmoNames,peopleNm  in cursor:
       if imgURL != 'https://ssl.pstatic.net/static/movie/2012/06/dft_img120x150.png' and imgURL != 'https://ssl.pstatic.net/static/movie/2012/06/dft_img77x96_1.png':
           #st.write(peopleNm)
           #else :
           #st.write(peopleNm, peopleNmEn)
-          with col1 :
+          with col_list[j] :
               st.image(imgURL,width=150,)
-          with col2 :
-              st.write('')
-              st.write(peopleNm,peopleNmEn)
-              st.write('')
-              st.write('')
-              st.write('')
-              st.write('')
-              st.write('')
-              st.write('')
-              st.write('')
-              st.write('')
-              st.write('')
-              st.write('')
-              st.write('')
-
-          with col3 :
+              st.image(imgURL,width=150,)
               st.write(filmoNames)
+          j+=1
+
+              
 
       
