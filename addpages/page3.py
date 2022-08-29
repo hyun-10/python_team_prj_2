c107fdc009e67ce72e1ceec8720b01ddb11e2c1e
hyun-10 <111376655+hyun-10@users.noreply.github.com>
2022-08-29 10:14:55 +0900

    Update page3.py

--- addpages/page3.py
+++ addpages/page3.py
@@ -9,8 +9,8 @@ def app():
   quary_string = f"select peopleCd, peopleNmEn ,repRoleNm, peopleNm ,imgURL ,filmoNames ,peopleNm from movie_people_included_imgURL where peopleNm in ('{input}')"
   cursor.execute(quary_string)
   
-  col1, col2, col3= st.columns(3,gap="small")
-  col_list=[col1, col2,col3]
+  col1, col2, col3, col4, col5= st.columns(5,gap="small")
+  col_list=[col1, col2,col3, col4, col5]
   j=0
   for peopleCd, peopleNmEn ,repRoleNm, peopleNm ,imgURL ,filmoNames,peopleNm  in cursor:
       if imgURL != 'https://ssl.pstatic.net/static/movie/2012/06/dft_img120x150.png' and imgURL != 'https://ssl.pstatic.net/static/movie/2012/06/dft_img77x96_1.png' :
