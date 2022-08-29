49f269873d348cacdbee2ba88fc678e37657777b
hyun-10 <111376655+hyun-10@users.noreply.github.com>
2022-08-29 09:45:52 +0900

    Update page3.py

--- addpages/page3.py
+++ addpages/page3.py
@@ -18,6 +18,11 @@ def app():
           with col1 :
               st.image(imgURL,width=150,)
           with col2 :
+              st.write('')
+              st.write('')
+              st.write('')
+              st.write('')
+              st.write('')
               st.write(peopleNm,peopleNmEn)
           with col3 :
               st.write(filmoNames)
