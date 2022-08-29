8dca25c2588ea925e113e02d34944ba48a912526
hyun-10 <111376655+hyun-10@users.noreply.github.com>
2022-08-29 10:57:14 +0900

    Update page3.py

--- addpages/page3.py
+++ addpages/page3.py
@@ -10,7 +10,7 @@ def app():
   cursor.execute(quary_string)
   
   col1, col2, col3= st.columns(3)
-  my_expander = st.expander()
+  
   col_list=[col1, col2,col3]
   j=0
   for peopleCd, peopleNmEn ,repRoleNm, peopleNm ,imgURL ,filmoNames,peopleNm  in cursor:
@@ -19,14 +19,14 @@ def app():
           #else :
           #st.write(peopleNm, peopleNmEn)
           with col_list[0] :
-              my_expander.image(imgURL,width=150)
+              st.image(imgURL,width=150)
 
     
           with col_list[1] :
-              my_expander.write(peopleNm,peopleNmEn,use_column_width=800)
+              st.write(peopleNm,peopleNmEn,use_column_width=800)
 
           with col_list[2] :
-              my_expander.write(filmoNames,use_column_width=800)
+              st.write(filmoNames,use_column_width=800)
 
           j+=1
 
